fad4819515ea4879aa91e26a3c0196138604a157
tahmidradit <tahmid.radit@outlook.com>
2025-11-05 12:21:55 +0600

    Removed unused scoped

--- RapidERP/RapidERP.Infrastructure/Extentions/RegisterLifetimeServices.cs
+++ RapidERP/RapidERP.Infrastructure/Extentions/RegisterLifetimeServices.cs
@@ -7,24 +7,8 @@ public static class RegisterLifetimeServices
 {
     public static IServiceCollection AddScopedServices(this IServiceCollection services)
     {
-        //services.AddScoped<IStatusType, StatusTypeService>();
-        //services.AddScoped<IActionType, ActionTypeService>();
         services.AddScoped<IExportType, ExportTypeService>();
-        //services.AddScoped<IDocumentTypeService, DocumentTypeService>();
-        //services.AddScoped<ILanguage, LanguageService>();
-        //services.AddScoped<ILanguageLocalization, LanguageLocalizationService>();
-        //services.AddScoped<IOrganization, OrganizationService>();
-        //services.AddScoped<ICurrency, CurrencyService>();
-        //services.AddScoped<ICurrencyLocalization, CurrencyLocalizationService>();
-        //services.AddScoped<ICountry, CountryService>();
-        //services.AddScoped<ICountryLocalization, CountryLocalizationService>();
-        //services.AddScoped<IState, StateService>();
-        //services.AddScoped<IStateLocalization, StateLocalizationService>();
-        //services.AddScoped<IDepartment, DepartmentService>();
-        //services.AddScoped<IDepartmentLocalization, DepartmentLocalizationService>();
-        //services.AddScoped<IDesignation, DesignationService>();
-        //services.AddScoped<IDesignationLocalization, DesignationLocalizationService>();
-
+        
         return services;
     }
 }
