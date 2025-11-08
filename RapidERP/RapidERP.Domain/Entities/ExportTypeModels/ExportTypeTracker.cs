d084b5ab01c7e6c19ca886e71f5a346ef0763b89
tahmidradit <tahmid.radit@outlook.com>
2025-11-08 12:09:46 +0600

    Deleted Export Type Tracker , Language Type Tracker class

--- RapidERP/RapidERP.Domain/Entities/ExportTypeModels/ExportTypeTracker.cs
+++
@@ -1,7 +0,0 @@
-﻿using RapidERP.Domain.Entities.Shared;
-
-namespace RapidERP.Domain.Entities.ExportTypeModels;
-public class ExportTypeTracker : Base
-{
-    public string Description { get; set; }
-}
--- RapidERP/RapidERP.Domain/Entities/LanguageModels/LanguageTracker.cs
+++
@@ -1,8 +0,0 @@
-﻿using RapidERP.Domain.Entities.Shared;
-
-namespace RapidERP.Domain.Entities.LanguageModels;
-public class LanguageTracker : Base 
-{
-    public Language Language { get; set; }
-    public int LanguageId { get; set; }
-}
