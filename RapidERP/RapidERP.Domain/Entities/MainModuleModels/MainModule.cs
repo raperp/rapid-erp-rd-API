eff1967c2178bf885b06516a0cccf53025b73512
tahmidradit <tahmid.radit@outlook.com>
2025-11-26 00:29:27 +0600

    Main modules

--- RapidERP/RapidERP.Domain/Entities/MainModuleModels/MainModule.cs
+++ RapidERP/RapidERP.Domain/Entities/MainModuleModels/MainModule.cs
@@ -1,12 +1,11 @@
-﻿using RapidERP.Domain.Entities.LanguageModels;
-using RapidERP.Domain.Entities.Shared;
+﻿using RapidERP.Domain.Entities.Shared;
 
 namespace RapidERP.Domain.Entities.MainModuleModels;
 
 public class MainModule : BaseMaster
 {
-    public Language Language { get; set; }
-    public int LanguageId { get; set; }
+     
     public string Prefix { get; set; }
     public string IconURL { get; set; }
+    public string SetSerial { get; set; }
 }
