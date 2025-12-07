bfdfa8165fca7e214fbdc2a6c47705697cf5179e
tahmidradit <tahmid.radit@outlook.com>
2025-12-07 19:58:16 +0600

    ..

---
+++ RapidERP/RapidERP.Infrastructure/Extentions/RegisterAuthentication.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RapidERP.Infrastructure.Data;
+
+namespace RapidERP.Infrastructure.Extentions;
+
+public static class RegisterAuthentication
+{
+    public static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+
+
+        return services;
+    }
+}
