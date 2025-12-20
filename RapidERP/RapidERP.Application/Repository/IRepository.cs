623ab7b082733790ef8c4a4acf3ee9ecbe67b4a0
tahmidradit <tahmid.radit@outlook.com>
2025-12-21 00:56:21 +0600

    ..

--- RapidERP/RapidERP.Application/Repository/IRepository.cs
+++ RapidERP/RapidERP.Application/Repository/IRepository.cs
@@ -3,15 +3,17 @@ using RapidERP.Domain.Utilities;
 
 namespace RapidERP.Application.Repository;
 
-public interface IRepository  
+public interface IRepository<TEntity, PUTEntity> where TEntity : class
 {
-    //Task<dynamic> GetAll(int skip, int take);
-    //Task<dynamic> GetSingle(int id);
-    //Task<dynamic> GetHistory(int skip, int take);
-    Task<dynamic> FindById<TEntity>(TEntity entity) where TEntity : Master;
-    Task<dynamic> FindByName<TEntity>(TEntity entity) where TEntity : Master;
-    Task Add<TEntity>(TEntity entity) where TEntity : class;
-    Task Update<TEntity>(TEntity entity) where TEntity : class;
+    Task<RequestResponse> GetAll(int skip, int take, int pageSize);
+    Task<dynamic> GetSingle(int id);
+    Task<RequestResponse> GetHistory(int skip, int take, int pageSize);
+    Task<RequestResponse> CreateSingle(TEntity masterPOST);
+    Task<RequestResponse> CreateBulk(List<TEntity> masterPOSTs);
+    Task<RequestResponse> Update(PUTEntity masterPUT);
+    //Task<RequestResponse> Delete(int id);
+    Task<dynamic> SoftDelete(int id);
     Task CommitChanges();
+    IQueryable<TEntity> DbSet();
     //Task<dynamic> CreateBulk(List<TEntity> masterPOSTs);
 }
--- RapidERP/RapidERP.Infrastructure/Repository/Repository.cs
+++ RapidERP/RapidERP.Infrastructure/Repository/Repository.cs
@@ -1,42 +1,94 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1;
 using RapidERP.Application.Repository;
-using RapidERP.Domain.Entities.Shared;
+using RapidERP.Domain.Utilities;
 using RapidERP.Infrastructure.Data;
 
 namespace RapidERP.Infrastructure.Repository;
 
-public class Repository(RapidERPDbContext context) : IRepository
+public class Repository<T, Q> : IRepository<T, Q> where T : class
 {
-    public async Task CommitChanges()
+    private readonly RapidERPDbContext context;
+    private readonly DbSet<T> dbSet;
+    public Repository(RapidERPDbContext context)
     {
-        await context.SaveChangesAsync();
+        this.context = context;
+        dbSet = context.Set<T>();
     }
 
-    public async Task Add<TEntity>(TEntity entity) where TEntity : class
+    public Task CommitChanges()
     {
-        await context.Set<TEntity>().AddAsync(entity);         
+        throw new NotImplementedException();
     }
 
-    public async Task Update<TEntity>(TEntity entity) where TEntity : class
+    public Task<RequestResponse> CreateBulk(List<T> masterPOSTs)
     {
-        if (context.ChangeTracker.HasChanges())
-        {
-            await CommitChanges();
-        }
+        throw new NotImplementedException();
     }
 
-    public async Task<dynamic> FindById<TEntity>(TEntity entity) where TEntity : Master
+    public Task<RequestResponse> CreateSingle(T masterPOST)
     {
-        var record = await context.Set<TEntity>().FindAsync(entity.Id)
-        ?? throw new ApplicationException("Record not found"); 
-        return record;
+        throw new NotImplementedException();
     }
 
-    public async Task<dynamic> FindByName<TEntity>(TEntity entity) where TEntity : Master
+    public IQueryable<T> DbSet()
     {
-        var record = await context.Set<TEntity>().FindAsync(entity.Name)
-        ?? throw new ApplicationException("Record not found");
-        return record;
+        return dbSet;
     }
+
+    public Task<RequestResponse> GetAll(int skip, int take, int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RequestResponse> GetHistory(int skip, int take, int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<dynamic> GetSingle(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<dynamic> SoftDelete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RequestResponse> Update(Q masterPUT)
+    {
+        throw new NotImplementedException();
+    }
+    //public async Task CommitChanges()
+    //{
+    //    await context.SaveChangesAsync();
+    //}
+
+    //public async Task Add<TEntity>(TEntity entity) where TEntity : class
+    //{
+    //    await context.Set<TEntity>().AddAsync(entity);         
+    //}
+
+    //public async Task Update<TEntity>(TEntity entity) where TEntity : class
+    //{
+    //    if (context.ChangeTracker.HasChanges())
+    //    {
+    //        await CommitChanges();
+    //    }
+    //}
+
+    //public async Task<dynamic> FindById<TEntity>(TEntity entity) where TEntity : Master
+    //{
+    //    var record = await context.Set<TEntity>().FindAsync(entity.Id)
+    //    ?? throw new ApplicationException("Record not found"); 
+    //    return record;
+    //}
+
+    //public async Task<dynamic> FindByName<TEntity>(TEntity entity) where TEntity : Master
+    //{
+    //    var record = await context.Set<TEntity>().FindAsync(entity.Name)
+    //    ?? throw new ApplicationException("Record not found");
+    //    return record;
+    //}
 }
